fe876457909c1b4f47f27fd2b9ecc6967cce4faa
avt2255 <avt5067@gmail.com>
2022-07-01 15:56:03 +0100

    banner setup

---
+++ src/Constants/Constants.js
@@ -0,0 +1,6 @@
+
+export const baseUrl = 'https://api.themoviedb.org/3';
+
+export const API_KEY = '9e39ffbae58453f41035cb9524bfa226';
+
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
